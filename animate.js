692f0b921e860e874022f0f408001a677e8bebec
FoxDelioncourt <fox.delioncourt@gmail.com>
2018-11-03 10:50:40 +0400

    Astrolabs-Porfolio

---
+++ animate.js
@@ -0,0 +1,42 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   //Intitial page load animation
+//   anime
+//     .timeline({
+//       easing: "easeOutExpo"
+//     })
+//     .add({
+//       targets: ".hamburger",
+//       translateY: [-1000, 0]
+//     })
+//     .add({
+//       targets: ".about-link",
+//       opacity: [0, 1],
+//       offset: "-=500"
+//     })
+//     .add({
+//       targets: ".projects-link",
+//       opacity: [0, 1],
+//       offset: "-=900"
+//     })
+//     .add({
+//       targets: ".passions-link",
+//       opacity: [0, 1],
+//       offset: "-=900"
+//     })
+//     .add({
+//       targets: ".contact-link",
+//       opacity: [0, 1],
+//       offset: "-=900"
+//     })
+//     .add({
+//       targets: ".left-container",
+//       translateY: [-2000, 0],
+//       // opacity: [0, 1],
+//       offset: "-=700"
+//     });
+//   // .add({
+//   //   targets: ".right-container",
+//   //   translateY: [2000, 0],
+//   //   offset: "-=700"
+//   // });
+// });
